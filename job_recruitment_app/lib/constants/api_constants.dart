3c8d6cd0baaf1e1be46f3ea93e84b5bbb71e5bb8
IT20272654 <shavindurajapaksha@gmail.com>
2026-01-14 21:29:30 +0530

    Add ApiConstants class for managing API endpoint constants

---
+++ job_recruitment_app/lib/constants/api_constants.dart
@@ -0,0 +1,10 @@
+class ApiConstants {
+  static const String baseUrl = 'http://10.0.2.2:5000/api';
+
+  static const String register = '/auth/register';
+  static const String login = '/auth/login';
+  static const String jobs = '/jobs';
+  static const String myJobs = '/jobs/my-jobs';
+  static const String applications = '/applications';
+  static const String myApplications = '/applications/my-applications';
+}
